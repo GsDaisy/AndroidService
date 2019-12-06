fdbc8a20049bc900a0bc381dcfa2bd1c7d052acb
GsDaisy <dbstndud0422@naver.com>
2019-12-06 10:04:12 +0900

    [Kotlin:Modify]Test the Service

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,24 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    internal lateinit var editText: EditText
-    internal lateinit var btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText = findViewById(R.id.editText) as EditText
-        btn = findViewById(R.id.btn)
         btn.setOnClickListener {
-            val name = editText.text.toString()
+            val name = editText.toString()
 
             val intent = Intent(applicationContext, MyService::class.java)
             intent.putExtra("command", "show")
